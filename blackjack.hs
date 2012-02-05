eebbd275e5fa4f1417e5fc83552f160e940e37b9
Wilfred Hughes <wilfred@potatolondon.com>
2012-02-05 01:32:37 -0800

    adding basic dealing functionality

--- blackjack.hs
+++ blackjack.hs
@@ -1,4 +1,9 @@
-
+import System.Random
 
 data Card = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine |
             Ten | Jack | Queen | King deriving Show
+
+dealOneCard [] = error "No cards left in deck"
+dealOneCard deck = do
+  randomCardIndex <- randomRIO (0, length deck - 1)
+  return (deck !! randomCardIndex)
