c8543cea0d163fcb4ea6c4d6f2674be7ec3c72c5
Wilfred Hughes <wilfred@potatolondon.com>
2012-02-05 16:16:08 -0800

    refactoring dealOneCard

--- blackjack.hs
+++ blackjack.hs
@@ -3,10 +3,11 @@ import System.Random
 data Card = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine |
             Ten | Jack | Queen | King deriving Show
 
+dealOneCard :: [Card] -> IO Card
 dealOneCard [] = error "No cards left in deck"
 dealOneCard deck = do
   randomCardIndex <- randomRIO (0, length deck - 1)
-  return (deck !! randomCardIndex)  return (deck !! randomCardIndex)
+  return (deck !! randomCardIndex)
   
 cardValues :: Card -> [Int]
 cardValues Ace = [1, 11]
