ca8ffe04a04acab9da1ce67cf692a63bd83f0c04
Wilfred Hughes <wilfred@potatolondon.com>
2012-02-05 16:16:57 -0800

    allow the value of a hand to be calculated

--- blackjack.hs
+++ blackjack.hs
@@ -2,6 +2,7 @@ import System.Random
 
 data Card = Ace | Two | Three | Four | Five | Six | Seven | Eight | Nine |
             Ten | Jack | Queen | King deriving Show
+type Hand = [Card]
 
 dealOneCard :: [Card] -> IO Card
 dealOneCard [] = error "No cards left in deck"
@@ -21,3 +22,10 @@ cardValues Eight = [8]
 cardValues Nine = [9]
 cardValues _ = [10]
 
+-- work out the possible scores this hand could have. No concerns have
+-- been given to efficiency here.
+possibleHandTotals :: Hand -> [Int] -> [Int]
+possibleHandTotals [] totals = sort $ nub totals
+possibleHandTotals (card:cards) runningTotals =
+  possibleHandTotals cards newTotals
+  where newTotals = [total + value | total <- runningTotals, value <- cardValues card]
